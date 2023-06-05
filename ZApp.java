6122d2130cafd22b6151cacc3a503224abb9fe6f
olsenolav <o.olav@wustl.edu>
2023-06-05 12:11:32 -0400

    deleted duplicate files

--- ZApp.java
+++
@@ -1,6 +0,0 @@
-public class ZApp {
-        public static void main(String[] args) throws Exception {
-        ZfullStitch test1 = new ZfullStitch(1, 2, "test1");
-        test1.stitchAcross(1);
-    }
-}
